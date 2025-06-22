2d90caf7d400d58833a1077d5a500032e58000b1
bryan <b.e.acosta.angulo@tue.nl>
2025-06-22 14:30:47 +0200

    optimization scheme

---
+++ external_simple_optimization.py
@@ -0,0 +1,138 @@
+from pymoo.core.problem import StarmapParallelization
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.sampling.lhs import LHS
+from pymoo.operators.selection.rnd import RandomSelection
+from pymoo.operators.mutation.pm import PolynomialMutation
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.optimize import minimize
+from pymoo.core.problem import ElementwiseProblem
+import multiprocessing
+from scipy.optimize import root
+from pyDOE3 import *
+import json
+import pandas as pd
+import numpy as np
+
+from stackSimulator import *
+
+
+class stack_opt_problem(ElementwiseProblem):
+    def __init__(self, **kwargs):
+        # stack parameters
+        self.N = 350 # number of cells
+        self.j_load = 4000 # A/m^2 
+        # tau, Dm_in, Dch_in, Lch_in, Lch_out, Dch_L, Dch_k, Dm_L, Dm_k
+        self.parameters = {"tau": [], "Dm_in": [], "Dch_in": [], "Lch_in": [],
+                           "Lch_out": [], "Dch_out": [], "Dm_out": []}
+        # rho electrolyte
+        self.p_op = 1.01325 * 1e5 # Pa
+        self.rho_w = 1250 # kg/m^3
+        self.mu_w = 0.001 # Pa*s
+        # max F
+        self.max_F = 1e2
+        self.min_F = 1e10 # initial value
+        # cell parameters
+        self.H = 1.5 # m
+        self.thickness = 5e-3 # m
+        # boundaries
+        xl_ = np.array([80, 5e-2, 6e-3, 0.5, 0.3, 3e-2, 1e-1]) # tau, Dm_in, Dch_in, Lch_in, Dch_L, Dch_k, Dm_L, Dm_k 
+        xu_ = np.array([400, 2e-1, 1.2e-2, 2., 1., 4e-2, 4e-1]) # tau, Dm_in, Dch_in, Lch_in, Dch_L, Dch_k, Dm_L, Dm_k
+        super().__init__(n_var=7, n_obj=2, xl=xl_, xu=xu_, **kwargs)
+        # filename
+        self.path = "templates/"
+            
+    def compute_shunt_current_batch(self, filename, x):
+        try:
+            self.set_system(filename, x)
+            stack = stackProblem("templates/mod_"+filename, "Colebrook")
+            stack.solve_problem()
+            cta_dp_ = abs((stack.Cell.cathode_side.p - stack.Cell.anode_side.p).mean()) * 1e-2 # mbar
+            shunt_rate_ = 100 * (1 - stack.stack_current_density() / stack.Conditions.j_load).mean() # %
+        except:
+            cta_dp_ = 1e8
+            shunt_rate_ = self.max_F * 1e6
+        return [shunt_rate_, cta_dp_] # shunt rate, cathodic pressure drop
+    
+        
+    def _evaluate(self, x, out, *args, **kwargs):
+        filename = "optim_four_manifolds.json"
+        out["F"] = self.compute_shunt_current_batch(filename, x)
+        # print and store local best solution 
+        if out["F"][0] < self.min_F and out["F"][-1] < 5: 
+            self.min_F = out["F"][0]
+            print("shunt rate: ", out["F"][0], " %")
+            for key, value in zip(self.parameters.keys(), x):
+                self.parameters[key] = [value]
+            df = pd.DataFrame(self.parameters)
+            df.to_csv("external_optim_params/tmp_optimal_paramters.csv", index=False)  
+    
+    def set_system(self, filename, x):
+        n_cell = self.N # number of cells
+        H = self.H # cell height
+        width = self.thickness # cell width
+        [tau, Dm_in, Dch_in, Lch, Lch_out, Dch_out, Dm_out] = x
+        # set parameters
+        Q_in = n_cell * width * np.pi / 4 * H**2 / tau # m^3/s
+        As = np.pi / 4 * H**2 # m^2
+        with open(self.path + filename, "r") as file:
+            data = json.load(file)
+        # set number of cells
+        data["settings"]["N"]["value"] = int(n_cell)
+        # set load 
+        data["conditions"]["j_load"]["value"] = self.j_load
+        # set flow rate
+        data["conditions"]["Q_in"]["value"] = [float(Q_in), float(Q_in)]
+        # set pressure 
+        data["conditions"]["p_out"]["value"] = float(self.p_op) # Pa
+        # set cell parameters
+        data["cell"]["height"]["value"] = float(H) # height
+        data["cell"]["length"]["value"] = float(width) # width
+        data["cell"]["As"]["value"] = float(As) # surface area
+        # set inlet channels
+        data["channel"][0]["diameter"]["value"] = float(Dch_in)
+        data["channel"][0]["length"]["value"] = float(Lch)
+        data["channel"][1]["diameter"]["value"] = float(Dch_in)
+        data["channel"][1]["length"]["value"] = float(Lch)
+        # set inlet manifolds
+        data["manifold"][0]["diameter"]["value"] = float(Dm_in)
+        data["manifold"][1]["diameter"]["value"] = float(Dm_in)
+        # set outlet channels
+        data["channel"][2]["diameter"]["value"] = float(Dch_out)
+        data["channel"][2]["length"]["value"] = float(Lch_out)
+        data["channel"][3]["diameter"]["value"] = float(Dch_out)
+        data["channel"][3]["length"]["value"] = float(Lch_out)
+        # set outlet manifolds
+        data["manifold"][2]["diameter"]["value"] = float(Dm_out)
+        data["manifold"][3]["diameter"]["value"] = float(Dm_out)
+        # update json file
+        with open("templates/mod_"+filename, "w") as file:
+            json.dump(data, file, indent=4)
+        return 0
+
+
+if __name__=="__main__":
+    n_proccess = 10
+    pool = multiprocessing.Pool(n_proccess)
+    runner = StarmapParallelization(pool.starmap)
+    problem = stack_opt_problem(elementwise_runner=runner)
+    algorithm = NSGA2(pop_size=100, sampling=LHS())
+    res = minimize(problem, algorithm, termination=("n_gen", 100), seed=0, verbose=True, save_history=True)
+    print("elapsed time: ", res.exec_time)
+    # save results to excel file
+    for key, value in zip(problem.parameters.keys(), res.X):
+        problem.parameters[key] = [value]
+    df = pd.DataFrame(problem.parameters)
+    df.to_csv("external_optim_params/params_I_1bar.csv", sep=",", index=False)
+    # save hystorical
+    n_evals = []             # corresponding number of function evaluations
+    hist_F = []              # the objective space values in each generation
+    for algo in res.history:
+        # function evaluations
+        n_evals.append(algo.evaluator.n_eval)
+        
+        # retrieve optimum
+        opt = algo.opt
+        hist_F.append(float(opt.get("F")[0,0]))
+
+    history_df = pd.DataFrame({"n_evals": n_evals, "F": hist_F})
+    history_df.to_csv("external_optim_params/history_I_1bar.csv", sep=",", index=False)
---
+++ general_optimizer.py
@@ -0,0 +1,54 @@
+from pymoo.core.problem import StarmapParallelization
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.sampling.lhs import LHS
+from pymoo.optimize import minimize
+from simple_optimization import stack_opt_problem as sop
+from single_simple_optimization import stack_opt_problem as ssop
+from external_simple_optimization import stack_opt_problem as esop
+import multiprocessing
+import pandas as pd
+import numpy as np
+
+def map_optimization(problem, p_vals, H_vals):
+    out_dict = {"Y": [], "cta_dp": [], "p": [], "H": []}
+    for key, value in zip(problem.parameters.keys(), problem.parameters.values()):
+        out_dict[key] = []
+    for p_ in p_vals:
+        for H_ in H_vals:
+            problem.p_op = p_ # Pa
+            problem.H = H_ # m
+            algorithm = NSGA2(pop_size=50, sampling=LHS())
+            res = minimize(problem, algorithm, termination=("n_gen", 20), seed=0, verbose=True, save_history=True) 
+            print("elapsed time: ", res.exec_time)
+            z = np.where(res.F[0,:] < 5)
+            zz = np.where(res.F[:,0] == min(res.F[:,0]))
+            F = res.F[zz]
+            X = res.X[zz]
+            for key, value in zip(out_dict.keys(), np.concatenate([F[0], [p_], [H_], X[0]])):
+                out_dict[key].append(value)
+    return out_dict
+
+# internal manifolds with separate inlets
+p_vals = np.array([5, 50]) * 1e5 # Pa
+H_vals = np.array([0.7, 1.6]) # m
+# parallelization scheme
+n_process = 10
+pool = multiprocessing.Pool(n_process)
+runner = StarmapParallelization(pool.starmap)
+problem = sop(elementwise_runner=runner)
+out_dict = map_optimization(problem, p_vals, H_vals)
+df = pd.DataFrame(out_dict)
+df.to_csv("internal_optim_params/optimization_map.csv", index=False)
+# single manifold with separate inlets
+problem = ssop(elementwise_runner=runner)
+out_dict = map_optimization(problem, p_vals, H_vals)
+df = pd.DataFrame(out_dict)
+df.to_csv("internal_optim_params_single/optimization_map.csv", index=False)
+# external manifolds with separate inlets
+p_vals = np.array([1.015]) * 1e5 # Pa
+H_vals = np.array([0.7, 1.6]) # m
+problem = esop(elementwise_runner=runner)
+out_dict = map_optimization(problem, p_vals, H_vals)
+df = pd.DataFrame(out_dict)
+df.to_csv("external_optim_params/optimization_map.csv", index=False)
+print("Optimization completed and results saved to CSV files.")
